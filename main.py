2b8a2ab4dc238a77644dfad84e64ec9fc039c32e
Adam <agm500300@gmail.com>
2017-09-24 16:19:18 -0500

    made page return the encrypted text in the textarea

--- main.py
+++ main.py
@@ -10,26 +10,26 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form action="/" method="POST">
             <label for="rotate-by">Rotate by:</label>
             <input id="rotate-by" type="text" name="rot" value='0' />
-            <textarea name="text"></textarea>
+            <textarea name="text">{0}</textarea>
             <input type="submit">
         </form>
     </body>
@@ -38,13 +38,13 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rotate = int(request.form['rot'])
     userstring = request.form['text']
-    encryptstring = '<h1>' + rotate_string(userstring,rotate) + '</h1>'
-    return encryptstring
+    encryptstring = rotate_string(userstring,rotate)
+    return form.format(encryptstring)
 app.run()
